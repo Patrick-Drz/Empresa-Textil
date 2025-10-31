de6fbd23bd4fa3d338c085211cc817421e0ea3a8
Patrick-Drz <patrickdelaguilardz@gmail.com>
2025-10-30 20:52:45 -0500

    correciones pom

--- src/main/java/com/empresatextil/ejercicio4/config/SecurityConfig.java
+++ src/main/java/com/empresatextil/ejercicio4/config/SecurityConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer; // Asegúrate de importar esto
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer; 
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console; // Y esto
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console; 
 
 @Configuration
 @EnableWebSecurity
