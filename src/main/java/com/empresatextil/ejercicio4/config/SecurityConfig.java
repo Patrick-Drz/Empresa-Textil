28f02131bd3c52a06960bfa3e129e37693e367d9
Patrick-Drz <patrickdelaguilardz@gmail.com>
2025-10-30 19:12:22 -0500

    h2-console

--- src/main/java/com/empresatextil/ejercicio4/config/SecurityConfig.java
+++ src/main/java/com/empresatextil/ejercicio4/config/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer; // Asegúrate de importar esto
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console; // Y esto
 
 @Configuration
 @EnableWebSecurity
@@ -14,21 +17,31 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((authorize) -> authorize
+            
+                .requestMatchers(toH2Console()).permitAll() 
+                
                 .requestMatchers("/login").permitAll()
                 
                 .anyRequest().authenticated() 
             )
             .formLogin(form -> form
-                .loginPage("/login") 
-                .loginProcessingUrl("/login") 
-                .defaultSuccessUrl("/home", true) 
-                .permitAll() 
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/home", true)
+                .permitAll()
             )
             .logout(logout -> logout
-                .logoutUrl("/logout") 
-                .logoutSuccessUrl("/login?logout") 
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
                 .permitAll()
+            )
+            .csrf(csrf -> csrf
+                .ignoringRequestMatchers(toH2Console())
+            )
+            .headers(headers -> headers
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
             );
+
         return http.build();
     }
 }
